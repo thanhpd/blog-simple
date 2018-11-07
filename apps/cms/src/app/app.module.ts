00e1a5e75fd4542f21c5912926921be4261e3007
thanhpd <thanhpd.mac@gmail.com>
2018-11-07 21:08:05 +0700

    chore: project setup

---
+++ apps/cms/src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { NxModule } from '@nrwl/nx';
+import { RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+
+@NgModule({
+  declarations: [AppComponent, LoginComponent],
+  imports: [
+    BrowserModule,
+    NxModule.forRoot(),
+    RouterModule.forRoot([], { initialNavigation: 'enabled' })
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
